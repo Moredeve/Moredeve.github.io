cd309327602b8d1ca5276a75d63b840235e4e01c
Gabriel Vieira Flores <48156370+gabrielvieiraf@users.noreply.github.com>
2021-11-01 21:41:04 -0300

    Update main.js

--- login/js/main.js
+++ login/js/main.js
@@ -48,7 +48,7 @@
             }
             else{
                 if ($("[name=pass]").val() == "Beca_123!"){
-                    window.location.href = "http://moredeve.com/";
+                    window.location = "http://moredeve.com/";
                     return false;
                 }
             }
